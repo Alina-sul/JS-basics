3e0258edc78c9be0a63e9403048a4fa3b4c73e9b
Alina-Sul <alina.sulkovsky@gmail.com>
2019-08-07 08:21:50 +0300

    statements - break

--- methods/statements.js
+++ methods/statements.js
@@ -1,7 +1,14 @@
 document.getElementById('title').innerHTML = "METHODS";
-document.getElementById('defaultData').innerHTML = "";
+arr = [1,2,3,4,5,6,7,8];
+let result = "";
+document.getElementById('defaultData').innerHTML = "arr: " + arr;
 
-
+//break
+result += "<u>break: loop for if arr[i] = 4 <br></u>"
+for (var i = 0; i < arr.length ; i++){
+  if(arr[i] === 4) { break; }
+  result += arr[i] + "<br>";
+}
 
 
 
